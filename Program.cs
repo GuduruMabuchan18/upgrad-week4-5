c09279edf121ebf54e79015476edac4f9465372e
GuduruMabuchan18 <mabuchanmabuchan01@gmail.com>
2026-04-02 18:29:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class BankAccount
+{
+    // Private field (hidden)
+    private double balance;
+
+    // Deposit method
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            balance += amount;
+            Console.WriteLine("Deposited: " + amount);
+        }
+        else
+        {
+            Console.WriteLine("Invalid deposit amount");
+        }
+    }
+
+    // Withdraw method
+    public void Withdraw(double amount)
+    {
+        if (amount > balance)
+        {
+            Console.WriteLine("Insufficient balance!");
+        }
+        else
+        {
+            balance -= amount;
+            Console.WriteLine("Withdrawn: " + amount);
+        }
+    }
+
+    // Get balance method
+    public double GetBalance()
+    {
+        return balance;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount acc = new BankAccount();
+
+        acc.Deposit(1000);
+        acc.Withdraw(300);
+
+        Console.WriteLine("Current Balance = " + acc.GetBalance());
+    }
+}
